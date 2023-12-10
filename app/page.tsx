bf66148c87a6cde51a22a1e74cf068345c963c33
Ivan Woogue <ivanwoogue2@gmail.com>
2023-12-10 23:22:13 +0800

    section header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import First from './sections/01-Founding_of_KKK/01-Founding_of_KKK'
+import Second from './sections/02-Implication_of_Wealthy_Filipinos'
 
 export default function Home() {
   return (
     <div>
-      <First/>
+      <Second/>
     </div>  
   )
 }
---
+++ app/sections/02-Implication_of_Wealthy_Filipinos/index.tsx
@@ -0,0 +1,15 @@
+import SectionHeader from "@/common/SectionHeader"
+import { title } from "process"
+
+export default function Second() {
+    return (
+        <div>
+            <SectionHeader 
+                headerInfo={{
+                    title: "Implication of Wealthy Filipinos by the Katipunan", 
+                    subtitle:"June, 1896", 
+                    description:""
+                }}/>
+        </div>
+    )
+}
