a043de35fee1239df2a015651c13c720ed7f3bd5
Frooge <jaantaros@gmail.com>
2023-12-11 20:28:32 +0800

    add first and third

---
+++ app/sections/03-Spanish_Discovery_of_the_Katipunan/index.tsx
@@ -0,0 +1,16 @@
+import CommonFramerMotion from '@/common/CommonFramerMotion'
+import SectionHeader from '@/common/SectionHeader'
+import React from 'react'
+
+export default function Third() {
+  return (
+    <CommonFramerMotion>
+            <SectionHeader 
+                headerInfo={{
+                    title: "Spanish “Discovery” of the Katipunan", 
+                    subtitle:"1896", 
+                    description: ""
+                }}/>
+    </CommonFramerMotion>
+  )
+}
