89c0388e474469eebeefae130fbc2e680dfea6ed
Ivan Woogue <ivanwoogue2@gmail.com>
2023-12-11 08:27:58 +0800

    added flip

--- app/sections/02-Implication_of_Wealthy_Filipinos/index.tsx
+++ app/sections/02-Implication_of_Wealthy_Filipinos/index.tsx
@@ -4,50 +4,64 @@ import SectionHeader from "@/common/SectionHeader"
 import { title } from "process"
 import Image from "next/image"
 import { motion } from "framer-motion"
+import ReactCardFlip from 'react-card-flip';
+import { useState } from "react"
 
 export default function Second() {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault()
+        setIsFlipped(!isFlipped)
+    }
+
     return (
-        <div>
+        <motion.div
+            variants={{
+                hidden: {
+                opacity: 0,
+                y: -20,
+                },
+    
+                visible: {
+                opacity: 1,
+                y: 0,
+                },
+            }}
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 1, delay: 0.1 }}
+            viewport={{ once: true }}>
             <SectionHeader 
                 headerInfo={{
                     title: "Implication of Wealthy Filipinos by the Katipunan", 
                     subtitle:"June, 1896", 
                     description:""
-                }}/>
+            }}/>                    
+            <Image
+                src="/img/andres-bonifacio.png"
+                height={300}
+                width={300}
+                alt="Andres Bonifacio"
+            />
+            <Image
+                src="/img/pio-valenzuela.png"
+                height={300}
+                width={300}
+                alt="Pio Valenzuela"
+            />  
+            <ReactCardFlip 
+                isFlipped={isFlipped} flipDirection="vertical">
+                <div>
+                    This is the front of the card.
+                    <button onClick={handleFlip}>Click to flip</button>
+                </div>
 
-            <motion.div
-                variants={{
-                    hidden: {
-                      opacity: 0,
-                      y: -20,
-                    },
-          
-                    visible: {
-                      opacity: 1,
-                      y: 0,
-                    },
-                  }}
-                  initial="hidden"
-                  whileInView="visible"
-                  transition={{ duration: 1, delay: 0.1 }}
-                  viewport={{ once: true }}
-                >
-                <Image
-                    src="/img/andres-bonifacio.png"
-                    height={300}
-                    width={300}
-                    alt="Andres Bonifacio"
-                />
-                <Image
-                    src="/img/pio-valenzuela.png"
-                    height={300}
-                    width={300}
-                    alt="Pio Valenzuela"
-                />
-
-
-            </motion.div>
-           
-        </div>
+                <div>
+                    This is the back of the card.
+                    <button onClick={handleFlip}>Click to flip</button>
+                </div>
+            </ReactCardFlip>          
+        </motion.div>
     )
 }
